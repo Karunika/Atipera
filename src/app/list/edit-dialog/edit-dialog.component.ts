f1f4f727da2fefcf692b1dd917c1ac41b48c4e45
Karunika <karunikatrip@gmail.com>
2024-09-23 05:36:29 +0200

    chore: form validation in edit

--- src/app/list/edit-dialog/edit-dialog.component.ts
+++ src/app/list/edit-dialog/edit-dialog.component.ts
@@ -10,19 +10,18 @@ import {
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { PeriodicElement } from '../../../interfaces/PeriodicElement';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { map } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
-interface PeriodicElement {
-    position: number,
-    name: string,
-    weight: number,
-    symbol: string
-}
 
 @Component({
     selector: 'app-edit-dialog',
     templateUrl: './edit-dialog.component.html',
     standalone: true,
     imports: [
+        AsyncPipe,
         MatFormFieldModule,
         MatInputModule,
         MatButtonModule,
@@ -36,6 +35,22 @@ export class EditDialog {
     readonly dialogRef = inject(MatDialogRef<EditDialog>);
     readonly data = inject<PeriodicElement>(MAT_DIALOG_DATA);
     readonly element = model<PeriodicElement>(this.data)
+
+    error$ = toObservable(this.element).pipe(
+        map(({ name, symbol, weight }) => {
+            return {
+                name: /^[a-zA-Z]{1,32}$/.test(name) ? '' : 'Invalid name',
+                symbol: /^[a-zA-Z]{1,3}$/.test(symbol) ? '' : 'Invalid symbol',
+                weight: weight <= 100 && weight >= 0 ? '' : 'Weight out of range'
+            }
+        })
+    )
+
+    nameError$ = this.error$.pipe(map(e => e.name))
+    symbolError$ = this.error$.pipe(map(e => e.symbol))
+    weightError$ = this.error$.pipe(map(e => e.weight))
+
+    isError$ = this.error$.pipe(map(e => Object.values(e).some(e => e)))
 
     onChangeInput(property: keyof PeriodicElement, event: Event) {
         this.element.update((previous: PeriodicElement) => ({
