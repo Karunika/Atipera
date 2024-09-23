750800472bc14f1e5d261fc3d084df5e7b9e3927
Karunika <karunikatrip@gmail.com>
2024-09-23 05:55:52 +0200

    chore: replaced bar with spinner

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -8,7 +8,8 @@ import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table'
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+// import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { debounceTime, distinctUntilChanged, map, Subject, switchMap, tap } from 'rxjs';
 import { rxState } from '@rx-angular/state';
 import { PeriodicElement } from '../../interfaces/PeriodicElement';
@@ -31,7 +32,8 @@ import { updateQuery } from './utils';
         MatTableModule,
         MatIconModule,
         MatCardModule,
-        MatProgressBarModule,
+        // MatProgressBarModule,
+        MatProgressSpinnerModule,
         QueryFilter,
     ]
 })
